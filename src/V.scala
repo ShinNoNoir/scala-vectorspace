ce8abfb335f97ce48a5394deb778b95920dc1ff9
Raynor Vliegendhart <shinnonoir+git@gmail.com>
2016-03-03 20:55:49 +0100

    Implement flatten for the vector type V

--- src/V.scala
+++ src/V.scala
@@ -29,6 +29,11 @@ class V[K: Numeric, A] private (val bag: HashMap[A, K]) {
     V(newBag)
   }
   
+  def flatten[B](implicit ev: A <:< V[K, B]): V[K, B] = {
+    val vs = for ((v,w) <- bag) yield v*w
+    vs.foldLeft(V.zero[K,B]) {_+_}
+  }
+  
   override def toString() = "V(%s)".format(bag)
 }
 
